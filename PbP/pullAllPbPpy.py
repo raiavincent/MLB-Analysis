5abdaea6df60ae139b32409ccb3bcdc39de10949
Vincent Raia <vincentraia1@gmail.com>
2021-11-10 22:13:02 -0500

    table schemas fixed

--- PbP/pullAllPbPpy.py
+++ PbP/pullAllPbPpy.py
@@ -9,6 +9,8 @@ from datetime import datetime
 from google.oauth2 import service_account
 import pandas_gbq
 import pandas as pd
+from pandas_gbq import schema
+from statcast_table_schema import statcast_schema
 
 project_id = 'valuesheet'
 table_id = 'MLB.statcast'
@@ -33,105 +35,113 @@ def pullData():
     dataPbP = dataPbP.rename(columns={'pitcher.1':'pitcher_1',
                                 'fielder_2.1':'fielder_2_1'})
     
-    dataPbP = pd.to_datetime(dataPbP['game_date'])
+    dataPbP = dataPbP.astype({'game_date':str})
     
-    dataPbP = dataPbP.astype({'id':int,
-                  'pitch_type':str,
-                  'release_speed':float,
-                  'release_pos_x':float,
-                  'release_pos_z':float,
-                  'player_name':str,
-                  'batter':int,
-                  'pitcher':int,
-                  'events':str,
-                  'description':str,
-                  'spin_dir':str,
-                  'spin_rate_deprecated':str,
-                  'break_angle_deprecated':str,
-                  'break_length_deprecated':str,
-                  'zone':int,
-                  'des':str,
-                  'game_type':str,
-                  'stand':str,
-                  'p_throws':str,
-                  'type':str,
-                  'home_team':str,
-                  'away_team':str,
-                  'hit_location':int,
-                  'bb_type':str,
-                  'balls':int,
-                  'strikes':int,
-                  'game_year':int,
-                  'pfx_x':float,
-                  'pfx_z':float,
-                  'plate_x':float,
-                  'plate_z':float,
-                  'on_3b':int,
-                  'on_2b':int,
-                  'on_1b':int,
-                  'outs_when_up':int,
-                  'inning':int,
-                  'inning_topbot':str,
-                  'hc_x':float,
-                  'hc_y':float,
-                  'tfs_deprecated':str,
-                  'tfs_zulu_deprecated':str,
-                  'fielder_2':int,
-                  'umpire':str,
-                  'sv_id':str,
-                  'vx0':float,
-                  'vy0':float,
-                  'vz0':float,
-                  'ax':float,
-                  'ay':float,
-                  'az':float,
-                  'sz_top':float,
-                  'sz_bot':float,
-                  'hit_distance_sc':int,
-                  'launch_speed':float,
-                  'launch_angle':int,
-                  'effective_speed':float,
-                  'release_spin_rate':int,
-                  'release_extension':float,
-                  'game_pk':int,
-                  'pitcher_1':int,
-                  'fielder_2_1':int,
-                  'fielder_3':int,
-                  'fielder_4':int,
-                  'fielder_5':int,
-                  'fielder_6':int,
-                  'fielder_7':int,
-                  'fielder_8':int,
-                  'fielder_9':int,
-                  'release_pos_y':float,
-                  'estimated_ba_using_speedangle':float,
-                  'estimated_woba_using_speedangle':float,
-                  'woba_value':float,
-                  'woba_denom':int,
-                  'babip_value':int,
-                  'iso_value':int,
-                  'launch_speed_angle':int,
-                  'at_bat_number':int,
-                  'pitch_number':int,
-                  'pitch_name':str,
-                  'home_score':int,
-                  'away_score':int,
-                  'bat_score':int,
-                  'fld_score':int,
-                  'post_away_score':int,
-                  'post_home_score':int,
-                  'post_bat_score':int,
-                  'post_fld_score':int,
-                  'if_fielding_alignment':str,
-                  'of_fielding_alignment':str,
-                  'spin_axis':int,
-                  'delta_home_win_exp':float,
-                  'delta_run_exp':float
-                  })
+    # dataPbP = dataPbP.drop(columns=['id'])
+    
+    # dataPbP = pd.to_datetime(dataPbP['game_date'])
+    
+    # dataPbP = dataPbP.astype({
+    #               'pitch_type':str,
+    #               'release_speed':float,
+    #               'release_pos_x':float,
+    #               'release_pos_z':float,
+    #               'player_name':str,
+    #               'batter':"Int64",
+    #               'pitcher':"Int64",
+    #               'events':str,
+    #               'description':str,
+    #               'spin_dir':str,
+    #               'spin_rate_deprecated':str,
+    #               'break_angle_deprecated':str,
+    #               'break_length_deprecated':str,
+    #               'zone':"Int64",
+    #               'des':str,
+    #               'game_type':str,
+    #               'stand':str,
+    #               'p_throws':str,
+    #               'home_team':str,
+    #               'away_team':str,
+    #               'hit_location':"Int64",
+    #               'bb_type':str,
+    #               'balls':"Int64",
+    #               'strikes':"Int64",
+    #               'game_year':"Int64",
+    #               'pfx_x':float,
+    #               'pfx_z':float,
+    #               'plate_x':float,
+    #               'plate_z':float,
+    #               'on_3b':"Int64",
+    #               'on_2b':"Int64",
+    #               'on_1b':"Int64",
+    #               'outs_when_up':"Int64",
+    #               'inning':"Int64",
+    #               'inning_topbot':str,
+    #               'hc_x':float,
+    #               'hc_y':float,
+    #               'tfs_deprecated':str,
+    #               'tfs_zulu_deprecated':str,
+    #               'fielder_2':"Int64",
+    #               'umpire':str,
+    #               'sv_id':str,
+    #               'vx0':float,
+    #               'vy0':float,
+    #               'vz0':float,
+    #               'ax':float,
+    #               'ay':float,
+    #               'az':float,
+    #               'sz_top':float,
+    #               'sz_bot':float,
+    #               'hit_distance_sc':"Int64",
+    #               'launch_speed':float,
+    #               'launch_angle':"Int64",
+    #               'effective_speed':float,
+    #               'release_spin_rate':"Int64",
+    #               'release_extension':float,
+    #               'game_pk':"Int64",
+    #               'pitcher_1':"Int64",
+    #               'fielder_2_1':"Int64",
+    #               'fielder_3':"Int64",
+    #               'fielder_4':"Int64",
+    #               'fielder_5':"Int64",
+    #               'fielder_6':"Int64",
+    #               'fielder_7':"Int64",
+    #               'fielder_8':"Int64",
+    #               'fielder_9':"Int64",
+    #               'release_pos_y':float,
+    #               'estimated_ba_using_speedangle':float,
+    #               'estimated_woba_using_speedangle':float,
+    #               'woba_value':float,
+    #               'woba_denom':"Int64",
+    #               'babip_value':"Int64",
+    #               'iso_value':"Int64",
+    #               'launch_speed_angle':"Int64",
+    #               'at_bat_number':"Int64",
+    #               'pitch_number':"Int64",
+    #               'pitch_name':str,
+    #               'home_score':"Int64",
+    #               'away_score':"Int64",
+    #               'bat_score':"Int64",
+    #               'fld_score':"Int64",
+    #               'post_away_score':"Int64",
+    #               'post_home_score':"Int64",
+    #               'post_bat_score':"Int64",
+    #               'post_fld_score':"Int64",
+    #               'if_fielding_alignment':str,
+    #               'of_fielding_alignment':str,
+    #               'spin_axis':"Int64",
+    #               'delta_home_win_exp':float,
+    #               'delta_run_exp':float,
+    #               'game_date':str
+    #               })
+    
+    # schema.generate_bq_schema(dataPbP)
+    
+    print(dataPbP.info(verbose=True))
     
     # list(dataPbP.columns)
     
-    # print(dataPbP.head)
+    # pr"Int64"(dataPbP.head)
     
     # save as csv for upload/distribution
     # dataPbP.to_csv('2015 Start PbP.csv')
@@ -139,7 +149,7 @@ def pullData():
     print('Uploading.')
     
     pandas_gbq.to_gbq(
-    dataPbP, table_id, project_id=project_id, if_exists='append')
+    dataPbP, table_id, project_id=project_id, if_exists='append',table_schema=statcast_schema)
 
 if __name__ == '__main__':
     pullData()
