f605d7cb4771a513cd6dbf6283084280c62227a4
Vincent Raia <vincentraia1@gmail.com>
2021-12-25 11:14:12 -0500

    chapman

--- Queries/15FastestPitches.sql
+++ Queries/15FastestPitches.sql
@@ -1,4 +1,11 @@
+-- Temp function to get year
+
+CREATE TEMP FUNCTION getYear(x DATE)
+    RETURNS INT64
+    AS (EXTRACT(year FROM x));
+
 SELECT
+    getYear(gd.game_date) AS year,
     pv.pitcher_full,
     pr.pitch_name,
     pv.release_speed,
@@ -9,6 +16,8 @@ FROM
     `valuesheet.MLB.pitching_view` AS pv
 INNER JOIN 
     `valuesheet.MLB.pitch_ref` AS pr ON pv.pitch_type = pr.pitch_type
+INNER JOIN 
+    `valuesheet.MLB.game_data` AS gd ON pv.game_pk = gd.game_pk
 ORDER BY
     release_speed DESC
 LIMIT 15 
